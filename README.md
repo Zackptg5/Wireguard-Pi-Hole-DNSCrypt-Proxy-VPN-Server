5230b979f8f4a94b877c982dbc2d9c5a80633e3c
Zackptg5 <5107713+Zackptg5@users.noreply.github.com>
2020-09-22 21:16:48 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,80 @@
+# Wireguard-Pi-Hole-Cloudflared/Unbound/DNSCrypt-VPN-Server
+Sets up your very own VPN server with my configs
+
+## Requirements
+* Debian 10 (newer will probably work too - check if you need unstable repos for latest wireguard and dnscrypt-proxy)
+  * Maybe Ubuntu - haven't tested it myself so you're on your own there
+
+## Options
+* Can change some of the variables in the beginning (keep all but devs if you're unsure):
+  * devs - name for each wireguard client/device conf you want to make
+  * initipaddr/initipaddr6 - ipv4/6 internal address you want to use for wireguard
+  * sshport - ssh port
+  * wgport - wireguard port
+  * cfrport - cloudflared port (optional)
+  * uport - unbound port (optional)
+  * dppport - dnscrypt-proxy port (optional)
+* Uncomment the port variable if you want unbound
+* Can change the unbound config (pi-hole.conf) how you want, don't change anything related to ips and ports though
+* Can change dnscrypt-proxy.toml how you want, don't change listen_addresses
+  * I recommend you change the servers and anonymized relays to the fastest for your location
+  * Alternatively, comment out 'server_names' and replace the anon routes with: `{ server_name='*', via=['anon-relay-1', 'anon-relay-2'] }` setting the anon relays to whichever you like (closest to your location would be most optimal)
+
+## Description
+* Changes ssh port number for security reasons
+* Locks down firewall with ufw to only allow relevant connections
+* Installs fail2ban and sets up a couple rules for security
+* Wireguard - vpn tunnel
+* Pi-hole - ad blocker
+* Cloudflared - DNS-Over-HTTPS proxy - encrypts dns requests - optional
+* Unbound - local recursive/caching dns resolver with dnssec support - optional
+* DNSCrypt-proxy - caches, encrypts, and annoymizes dns requests - optional
+
+## Configurations:
+* Just Pi-hole - barebones, pick a dns server in the pihole gui - you need to trust your isp and selected upstream DNS server(s)
+* Pi-hole + cloudflared - use cloudflare's DOH service - if you trust cloudflare more than your server's ISP
+* Pi-hole + Unbound - increases privacy and uses dnssec - only entity you need to trust is your server's ISP
+* Pi-hole + unbound + cloudflared
+* Pi-hole + dnscrypt-proxy - use whatever dns servers you want - encrypts + anonymizes requests - you don't need to trust anyone
+* Pi-hole + unbound + dnscrypt-proxy - same as above but with some local dns resolution via unbound - I don't see much point in this but it's here if you want it
+
+## Super Simplified and Probably Partly Incorrect How it Works 
+* Device connects to server via wireguard tunnel -> Pi-Hole filters out ads -> Unbound resolves DNS queries that it can (if you chose it) -> DNSCrypt encrypts, authenticates, and annonymizes dns requests being sent out of the server and back
+
+## Unbound + Dnscrypt-Proxy?
+* DNSSEC/Security: dnscrypt-proxy enforces dnssec/encrypts dns requests and is what communicates with the outside world. Unbound also enforces DNSSEC but since it forwards requests to dnscrypt-proxy, what unbound does here doesn't really matter (dnscrypt-proxy enforces DNSSEC too btw)
+* Privacy: thanks to anonymous relays, dnscrypt-proxy hides your IP so all outgoing dns requests aren't traced back to you. Once again, unbound doesn't really help here and has no equivalent function at the time of me writing this
+* You may want unbound for other reasons and so I made this optional
+* Bascially, the addition of anonymized relays negates the need for unbound
+
+## How to Install
+* ssh into your server as root
+* Place all of the files in this repo in your vps server
+* `chmod +x VPS_Setup.bash `
+* `bash VPS_Setup.bash`
+  * Follow script instructions - will require several reboots
+* Setup your lists in pi-hole
+
+## Other Notes
+* A QR Code for each profile will be outputted during setup. You can take a picture of it with the device you want to use from the wireguard app
+* The unbound config (pi-hole.conf) is pretty solid I think. Only thing you may want to change is some performance related variables like num-threads. Also note that enabling `auto-trust-anchor-file` prevented unbound service from starting regardless of forwarding or lack there of on my server. 
+* dnscrypt config (dnscrypt-proxy.toml) is set to use only dnscrypt servers with dnssec, no logging or filtering, and then annonymizes them. [See here for more details.](https://github.com/DNSCrypt/dnscrypt-proxy/wiki/Anonymized-DNS) Note that I was able to wildcard it because the anon relays either didn't have a corresponding public server at the time of writing this or do filtering of some kind and so their servers aren't used (such as cryptostorm). This was a big plus for me because dnscrypt automatically sorts and picks the one with the lowest latency. Feel free to enable DOH or customize these however you want. DOH will require some extra setup though
+* I have ipv6 enabled
+* See the comments at the top of the setup script for some tips on ssh key generation
+
+## Sources I Found Helpful Setting This All Up
+* [zzzkeil - Wireguard DNSCrypt Server Setup](https://github.com/zzzkeil/Wireguard-DNScrypt-VPN-Server)
+* [notasausage - Pi-hole Unbound Wireguard Setup](https://github.com/notasausage/pi-hole-unbound-wireguard)
+* [Unofficial Wireguard Docs](https://github.com/pirate/wireguard-docs)
+* [Pi-Hole Unbound Docs](https://docs.pi-hole.net/guides/unbound)
+* [SSH Key Pairing on Debian](https://devconnected.com/how-to-set-up-ssh-keys-on-debian-10-buster)
+* [Unbound DNS Guide](https://calomel.org/unbound_dns.html)
+* [Sample Unbound Config](https://gist.github.com/MatthewVance/5051bf45cfed6e4a2a2ed9bb014bcd72)
+* [Unbound Config Docs](https://nlnetlabs.nl/documentation/unbound/unbound.conf)
+* [Pi-Hole DNSCrypt 2 Docs](https://github.com/pi-hole/pi-hole/wiki/DNSCrypt-2.0)
+* [Anonymized DNS Docs](https://github.com/DNSCrypt/dnscrypt-proxy/wiki/Anonymized-DNS)
+* [Commonly Whitelisted Domains](https://discourse.pi-hole.net/t/commonly-whitelisted-domains/212)
+* [More Commonly Whitelisted Domains](https://github.com/anudeepND/whitelist)
+* [DNS Leak Test](https://dnsleaktest.com)
+* [IP Leak Test](https://ipleak.net)
+* [See @jedisct1 comment for my reasoning behind unbound/dnscrypt-proxy setup for security/privacy](https://www.reddit.com/r/privacytoolsIO/comments/98ggn4/unbound_recursive_or_dnscrypt/e4h5sre?utm_source=share&utm_medium=web2x&context=3)
---
+++ VPS_Setup.bash
@@ -0,0 +1,229 @@
+#!/bin/bash
+###### Notes ######
+## SSH Key Gen ####
+# To remove key from client: ssh-keygen -R <ipaddr>
+# To login after changing port: ssh <USER>@ipaddr -p $sshport
+# To generate ssh keys:
+# On client:
+# ssh-keygen -t rsa -b 4096 # just hit enter for password prompts
+# ssh-copy-id <USER>@ipaddr -p $sshport
+## See used ports ##
+# lsof -i -P -n
+####################
+
+if [[ $EUID -ne 0 ]]; then
+  echo "This script must be run as root" 
+  exit 1
+fi
+
+dir=$PWD
+inet=$(ip route show default | awk '/default/ {print $5}')
+ipaddr="$(hostname -I | awk '{print $1}')"
+ipaddr6="$(hostname -I | awk '{print $2}')"
+
+# User Setable Variables
+intipaddr="192.168.1"
+intipaddr6="2607:55:55:55"
+wgport=51820
+sshport=1024
+devs="dabeast zenvy pixel"
+# uport=5353
+dpport=5354
+# cfrport=5053
+
+# Setup
+# passwd
+# resize2fs "$(df -h | awk '{print $1}' | grep /dev/ | head -n1)" # Needed for my vps server
+echo "Updating and installing packages"
+echo "Keep installed copies if asked!"
+sleep 3
+apt update && apt upgrade -y
+apt install sudo fail2ban curl -y
+apt install speedtest-cli dnsutils htop -y # Optional for testing/monitoring purposes
+echo "Configuring security measures"
+sleep 1
+sed -i -e 's|maxretry = 5|maxretry = 3|' -e "s|^#ignoreip = .*|ignoreip = 127.0.0.1/8 ::1 $intipaddr.0/24 $intipaddr6::1 $ipaddr|" /etc/fail2ban/jail.conf
+systemctl enable fail2ban
+systemctl start fail2ban
+sed -ri -e 's/^#PermitEmptyPasswords .*|^PermitEmptyPasswords yes/PermitEmptyPasswords no/' -e "s/^#Port.*|^Port.*/Port $sshport/" /etc/ssh/sshd_config
+service sshd restart
+echo "Rerun this script after rebooting!"
+echo "ssh root@$ipaddr -p $sshport"
+sed -i -e "s/^ipaddr=.*/ipaddr=\"$ipaddr\"/" -e "s/^ipaddr6=.*/ipaddr6=\"$ipaddr6\"/" -e "s/^inet=.*/inet=$inet/" -e "/^# Setup/,/^exit 0/d" $0
+reboot
+exit 0
+
+echo "Setting up UFW"
+sleep 1
+apt install ufw -y
+# Wireguard rule
+ufw allow $wgport/udp
+# SSH rule
+ufw allow $sshport/any
+# Pi-hole rules
+ufw allow 80/tcp
+ufw allow 53/tcp
+ufw allow 53/udp
+ufw allow 67/tcp
+ufw allow 67/udp
+ufw allow 546:547/udp
+# Wireguard rules
+sed -i 's/DEFAULT_FORWARD_POLICY="DROP"/DEFAULT_FORWARD_POLICY="ACCEPT"/' /etc/default/ufw
+sed -i "1i# START WIREGUARD RULES\n# NAT table rules\n*nat\n:POSTROUTING ACCEPT [0:0]\n# Allow traffic from WIREGUARD client \n-A POSTROUTING -s $intipaddr.0/24 -o $inet -j MASQUERADE\nCOMMIT\n# END WIREGUARD RULES\n" /etc/ufw/before.rules
+sed -i "/# End required lines/a-A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT\n-A INPUT -p udp -m udp --dport $wgport -m conntrack --ctstate NEW -j ACCEPT\n-A INPUT -s $intipaddr.0/24 -p tcp -m tcp --dport 53 -m conntrack --ctstate NEW -j ACCEPT\n-A INPUT -s $intipaddr.0/24 -p udp -m udp --dport 53 -m conntrack --ctstate NEW -j ACCEPT\n-A FORWARD -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT\n-A FORWARD -i wg0 -o wg0 -m conntrack --ctstate NEW -j ACCEPT" /etc/ufw/before.rules
+sed -i "/-A ufw-before-input -p icmp --icmp-type echo-request -j ACCEPT/a# allow outbound icmp\n-A ufw-before-output -p icmp -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT\n-A ufw-before-output -p icmp -m state --state ESTABLISHED,RELATED -j ACCEPT\n" /etc/ufw/before.rules
+sed -i "1i# START WIREGUARD RULES\n# NAT table rules\n*nat\n:POSTROUTING ACCEPT [0:0]\n# Allow traffic from WIREGUARD client \n\n-A POSTROUTING -s $intipaddr6::/112 -o $inet -j MASQUERADE\nCOMMIT\n# END WIREGUARD RULES\n" /etc/ufw/before6.rules
+sed -i "/# End required lines/a-A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT\n-A INPUT -p udp -m udp --dport $wgport -m conntrack --ctstate NEW -j ACCEPT\n-A INPUT -s $intipaddr6::1/64 -p tcp -m tcp --dport 53 -m conntrack --ctstate NEW -j ACCEPT\n-A INPUT -s $intipaddr6::1/64 -p udp -m udp --dport 53 -m conntrack --ctstate NEW -j ACCEPT\n-A FORWARD -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT\n-A FORWARD -i wg0 -o wg0 -m conntrack --ctstate NEW -j ACCEPT" /etc/ufw/before6.rules
+
+echo "Configuring sysctl"
+sleep 1
+sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g' /etc/sysctl.conf
+sed -i 's/#net.ipv6.conf.all.forwarding=1/net.ipv6.conf.all.forwarding=1/g' /etc/sysctl.conf
+sed -i 's@#net/ipv4/ip_forward=1@net/ipv4/ip_forward=1@g' /etc/ufw/sysctl.conf
+sed -i 's@#net/ipv6/conf/default/forwarding=1@net/ipv6/conf/default/forwarding=1@g' /etc/ufw/sysctl.conf
+sed -i 's@#net/ipv6/conf/all/forwarding=1@net/ipv6/conf/all/forwarding=1@g' /etc/ufw/sysctl.conf
+
+# Set up unstable repo for wireguard and dnscrypt-proxy
+echo "deb http://deb.debian.org/debian/ unstable main" > /etc/apt/sources.list.d/unstable.list
+printf 'Package: *\nPin: release a=unstable\nPin-Priority: 90\n' >> /etc/apt/preferences.d/limit-unstable
+apt update -y
+
+echo "Setting up Pi-hole v5.0"
+echo "Select any dns server - it'll get changed by this script"
+sleep 3
+curl -sSL https://install.pi-hole.net | bash
+sed -i "s/domain_name_servers=.*/domain_name_servers=$ipaddr $ipaddr6/" /etc/dhcpcd.conf
+echo "Set password to whatever"
+sleep 1
+pihole -a -p
+# Required for DNSSEC to work
+echo "proxy-dnssec" > /etc/dnsmasq.d/02-dnscrypt.conf
+
+if [ "$uport" ]; then
+    echo "Setting up Unbound"
+    sleep 1
+    port=$uport
+    apt install unbound unbound-host -y
+    curl -o /var/lib/unbound/root.hints https://www.internic.net/domain/named.cache
+    chown -R unbound:unbound /var/lib/unbound
+    sed -i -e "s/<port>/$uport/" -e "s/<ipaddr>/$ipaddr/g" -e "s/<intipaddr>/$intipaddr/g" -e "s/<intipaddr6>/$intipaddr6/g" pi-hole.conf
+    mv -f pi-hole.conf /etc/unbound/unbound.conf.d/pi-hole.conf
+    # Fix for no dns for wireguard client issue
+    systemctl disable unbound
+    sed -i 's/^After=network.target/After=wg-quick@wg0.service/' /lib/systemd/system/unbound.service
+fi
+if [ "$cfrport" ]; then
+  echo "Setting up cloudflared"
+  sleep 1
+  [ "$uport" ] && sed -i "/^ *name:/a        forward-addr: 127.0.0.1@$cfrport#\n        forward-addr: ::1@$cfrport" /etc/unbound/unbound.conf.d/pi-hole.conf || port=$cfrport
+  wget https://bin.equinox.io/c/VdrWdbjqyF/cloudflared-stable-linux-amd64.deb
+  apt install ./cloudflared-stable-linux-amd64.deb && rm -f cloudflared-stable-linux-amd64.deb
+  cloudflared -v
+  mkdir /etc/cloudflared
+  echo -e "proxy-dns: true\nproxy-dns-port: $cfrport\nproxy-dns-upstream:\n  - https://1.1.1.1/dns-query\n  - https://1.0.0.1/dns-query\n  - https://[2606:4700:4700::1111]/dns-query\n  - https://[2606:4700:4700::1001]/dns-query" > /etc/cloudflared/config.yml
+  cloudflared service install
+  systemctl start cloudflared
+  # Setup weekly update check since not in repo
+  echo -e "cloudflared update\nsystemctl restart cloudflared" > /etc/cron.weekly/cloudflared-updater.sh
+  chmod +x /etc/cron.weekly/cloudflared-updater.sh
+  chown root:root /etc/cron.weekly/cloudflared-updater.sh
+elif [ "$dpport" ]; then
+  echo "Setting up dnscrypt"
+  sleep 1
+  [ "$uport" ] && sed -i "/^ *name:/a        forward-addr: 127.0.0.1@$dpport#\n        forward-addr: ::1@$dpport" /etc/unbound/unbound.conf.d/pi-hole.conf || port=$dpport
+  apt install -t unstable dnscrypt-proxy
+  mv -f dnscrypt-proxy.toml /etc/dnscrypt-proxy/dnscrypt-proxy.toml
+  sed -i "s/=127..*/=127.0.0.1:$dpport/g" /lib/systemd/system/dnscrypt-proxy.socket
+elif [ "$uport" ]; then
+  # Use cloudflare
+  sed -i "/^ *name:/a\        forward-addr: 1.1.1.1@53#cloudflare-dns.com\n        forward-addr: 1.0.0.1@53#cloudflare-dns.com" /etc/unbound/unbound.conf.d/pi-hole.conf
+fi
+
+# Now we can start it since forward addresses are set
+if [ "$uport" ]; then
+  systemctl enable unbound
+  systemctl start unbound
+fi
+
+echo "Setting up wireguard"
+sleep 1
+apt install wireguard qrencode -y
+umask 077
+cd /etc/wireguard
+
+# Server
+wg genpsk > preshared-key
+wg genkey | tee server-privkey | wg pubkey > server-pubkey
+echo "[Interface]
+Address = $intipaddr.1/24, $intipaddr6::1/64
+ListenPort = $wgport
+SaveConfig = true
+PrivateKey = $(<server-privkey)
+PostUp = iptables -A FORWARD -i wg0 -j ACCEPT; iptables -A FORWARD -o wg0 -j ACCEPT; iptables -t nat -A POSTROUTING -o $inet -j MASQUERADE
+PostDown = iptables -D FORWARD -i wg0 -j ACCEPT; iptables -D FORWARD -o wg0 -j ACCEPT; iptables -t nat -D POSTROUTING -o $inet -j MASQUERADE" > wg0.conf
+
+# Clients
+COUNT=2
+for i in $devs; do
+  wg genkey | tee $i-privkey | wg pubkey > $i-pubkey
+  echo "
+[Peer]
+# $i
+PublicKey = $(<$i-pubkey)
+PresharedKey = $(<preshared-key)
+AllowedIPs = $intipaddr.$COUNT/32, $intipaddr6::$COUNT/128
+PersistentkeepAlive = 60" >> wg0.conf
+
+  echo "[Interface]
+Address = $intipaddr.$COUNT/24, $intipaddr6::$COUNT/64
+MTU = 1500
+DNS = $ipaddr, $ipaddr6
+PrivateKey = $(<$i-privkey)
+
+[Peer]
+PublicKey = $(<server-pubkey)
+PresharedKey = $(<preshared-key)
+Endpoint = $ipaddr:$wgport
+AllowedIPs = 0.0.0.0/0, ::/0
+PersistentKeepalive = 60" > $i.conf
+
+  echo "$i:"
+  qrencode -t ansiutf8 < $i.conf
+  sleep 1
+  cp -f $i.conf $dir/$i.conf
+  COUNT=$((COUNT+1))
+done
+
+chown -R root:root *
+chmod -R og-rwx *
+umask 0022
+cd $dir
+systemctl enable wg-quick@wg0.service
+systemctl start wg-quick@wg0.service
+
+# Set DNS servers
+if [ "$port" ]; then
+  sed -ri "/^PIHOLE_DNS|^DNS_FQDN_REQUIRED|^DNS_BOGUS_PRIV|^DNSSEC|^DNSMASQ_LISTENING|^PIHOLE_INTERFACE/d" /etc/pihole/setupVars.conf
+  echo -e "PIHOLE_INTERFACE=$inet\nDNSMASQ_LISTENING=local\nDNS_FQDN_REQUIRED=false\nDNS_BOGUS_PRIV=false\nDNSSEC=false" >> /etc/pihole/setupVars.conf
+fi
+case $port in
+  $uport) echo -e "PIHOLE_DNS_1=127.0.0.1#$uport\nPIHOLE_DNS_2=::1#$uport" >> /etc/pihole/setupVars.conf;;
+  $cfrport) echo -e "PIHOLE_DNS_1=127.0.0.1#$cfrport\nPIHOLE_DNS_2=::1#$cfrport" >> /etc/pihole/setupVars.conf;;
+  $dpport) echo -e "PIHOLE_DNS_1=127.0.0.1#$dpport\nPIHOLE_DNS_2=::1#$dpport" >> /etc/pihole/setupVars.conf;;
+  *) sed -i -e "s/DNSMASQ_LISTENING=.*/DNSMASQ_LISTENING=local/g" -e "s/PIHOLE_INTERFACE=.*/PIHOLE_INTERFACE=$inet/g" /etc/pihole/setupVars.conf;;
+esac
+
+# Only allow those connected to vpn to access pi-hole
+echo -e '$HTTP["remoteip"] !~ "'$intipaddr'\." {\n  url.access-deny = ( "" )\n }' > /etc/lighttpd/external.conf
+pihole restartdns
+
+# Reload ufw
+ufw --force enable
+ufw reload
+
+echo "All Done!"
+echo "Cleaning up and rebooting!"
+sleep 1
+rm $0
+reboot
+exit 0
