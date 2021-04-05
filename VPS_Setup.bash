399c5b0ca84e8c46fc07aace12708b290fc626dd
Zackptg5 <5107713+Zackptg5@users.noreply.github.com>
2021-04-04 20:47:10 -0400

    Update sci-hub for v1.0

--- VPS_Setup.bash
+++ VPS_Setup.bash
@@ -239,8 +239,7 @@ if [ "$searx" ]; then
   sed -i "/image_proxy/a\ \nengines:\n  - name : wikipedia    engine : wikipedia    shortcut : wp    base_url : 'https://{language}.wikipedia.org/'    disabled : true\n  - name : bing\n    engine : bing\n    shortcut : bi\n    disabled : true" /etc/searx/settings.yml
   sed -i '/image_proxy/a\ \nenabled_plugins:\n  - "Open Access DOI rewrite"\n  - "Hash plugin"\n  - "HTTPS rewrite"\n  - "Infinite scroll"\n  - "Self Informations"\n  - "Search on category select"\n  - "Tracker URL remover"' /etc/searx/settings.yml
   # Set sci-hub to default, fix link (tw is dead)
-  sed -i "s/oadoi.org/sci-hub.do/g" /usr/local/searx/searx-src/searx/preferences.py
-  sed -i "s/sci-hub.tw/sci-hub.do/g" /usr/local/searx/searx-src/searx/settings.yml
+  sed -i -e "s/sci-hub.tw/sci-hub.do/g" -e "s|default_doi_resolver :.*|default_doi_resolver : 'sci-hub.do'|" /usr/local/searx/searx-src/searx/settings.yml
   systemctl restart uwsgi
 fi
 # Add custom domain name as redirect for main page
