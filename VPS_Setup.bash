5b7eba50b26114ae3de8358b95ae591b7b078635
Zackptg5 <5107713+Zackptg5@users.noreply.github.com>
2021-04-15 14:03:23 -0400

    fix spacing

--- VPS_Setup.bash
+++ VPS_Setup.bash
@@ -236,7 +236,7 @@ if [ "$searx" ]; then
   sed -i -e "s/secret_key : .*/secret_key : $(openssl rand -hex 16)/" -e 's/autocomplete : ".*" #/autocomplete : "google" #/' /etc/searx/settings.yml
   # Set dark theme, show advanced settings, and disable wikipedia (doesn't work) and bing by default
   sed -i "/image_proxy/a\ \nui:\n    advanced_search : True\n    theme_args :\n        oscar_style : logicodev-dark" /etc/searx/settings.yml
-  sed -i "/image_proxy/a\ \nengines:\n  - name : wikipedia    engine : wikipedia    shortcut : wp    base_url : 'https://{language}.wikipedia.org/'    disabled : true\n  - name : bing\n    engine : bing\n    shortcut : bi\n    disabled : true" /etc/searx/settings.yml
+  sed -i "/image_proxy/a\ \nengines:\n  - name : wikipedia\n    engine : wikipedia\n    shortcut : wp\n    base_url : 'https://{language}.wikipedia.org/'\n    disabled : true\n  - name : bing\n    engine : bing\n    shortcut : bi\n    disabled : true" /etc/searx/settings.yml
   sed -i '/image_proxy/a\ \nenabled_plugins:\n  - "Open Access DOI rewrite"\n  - "Hash plugin"\n  - "HTTPS rewrite"\n  - "Infinite scroll"\n  - "Self Informations"\n  - "Search on category select"\n  - "Tracker URL remover"' /etc/searx/settings.yml
   # Set sci-hub to default, fix link (tw is dead)
   sed -i -e "s/sci-hub.tw/sci-hub.do/g" -e "s|default_doi_resolver :.*|default_doi_resolver : 'sci-hub.do'|" /usr/local/searx/searx-src/searx/settings.yml
